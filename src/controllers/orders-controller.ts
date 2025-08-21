949862ef64cda2c6498f88b997c51b4b0de368a2
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-08-21 19:30:50 -0300

    feat:creating a method of create and validations for an order

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,3 +1,5 @@
+import { knex } from "@/database/knex";
+import { AppError } from "@/utils/app-error";
 import { Request, Response, NextFunction } from "express";
 import z from "zod";
 class OrdersController {
@@ -11,7 +13,26 @@ class OrdersController {
       const { table_session_id, product_id, quantity } = bodySchema.parse(
         req.body
       );
-      return res.status(201).json({ table_session_id, product_id, quantity });
+
+      const tableSession = await knex<TablesSessionsRepository>(
+        "tables_sessions"
+      )
+        .where({ id: table_session_id })
+        .first();
+
+      if (!tableSession) {
+        throw new AppError("Sessão não encontrada", 404);
+      }
+      if (tableSession.closed_at) {
+        throw new AppError("Sessão já encerrada", 404);
+      }
+
+      const product = await knex<ProductRepository>("products").where({ id: product_id }).first();
+      if (!product) {
+        throw new AppError("Produto não encontrado", 404);
+      }
+
+      return res.status(201).json(product);
     } catch (error) {
       next(error);
     }
