f9b16bac09f12d13bde9b9e736ece40fbd63fb17
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-08-22 13:53:55 -0300

    feat:creating a new table

--- src/controllers/tables-controller.ts
+++ src/controllers/tables-controller.ts
@@ -1,6 +1,7 @@
 import { AppError } from "@/utils/app-error";
 import { knex } from "../database/knex";
 import { Request, Response, NextFunction } from "express";
+import z from "zod";
 export class TablesController {
   async index(req: Request, res: Response, next: NextFunction) {
     try {
@@ -14,5 +15,21 @@ export class TablesController {
     } catch (error) {
       next(error);
     }
+  }
+  async create(req: Request, res: Response, next: NextFunction) {
+    const { table_number } = z
+      .object({
+        table_number: z.coerce.number().nonoptional(),
+      })
+      .parse(req.body);
+    const tableExist = await knex<TableRepository>("tables").where({
+      table_number,
+    });
+
+    if (tableExist.length > 0) {
+      throw new AppError("A mesa com esse número já existe", 400);
+    }
+    await knex<TableRepository>("tables").insert({ table_number });
+    return res.status(200).json({ message: "Mesa criada com sucesso" });
   }
 }
